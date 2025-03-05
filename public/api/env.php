1083a1e28ec487d4f7deddf54ed12efe21e6199e
bazileros <zalisilesithule@gmail.com>
2025-03-05 08:12:37 +0200

    [UPODATE] update @version1.0.6

--- public/api/env.php
+++ public/api/env.php
@@ -1,10 +1,24 @@
 <?php
 // Load environment variables from .user.ini if available
 $env_file = $_SERVER['DOCUMENT_ROOT'] . '/.user.ini';
+$loaded_env = false;
+
 if (file_exists($env_file)) {
     $env_vars = parse_ini_file($env_file);
-    foreach ($env_vars as $key => $value) {
-        putenv("$key=$value");
+    if ($env_vars !== false) {
+        foreach ($env_vars as $key => $value) {
+            putenv("$key=$value");
+        }
+        $loaded_env = true;
+    }
+}
+
+// Fallback to credentials.php if .user.ini couldn't be loaded
+if (!$loaded_env) {
+    // Path assuming API is in public_html/api/ and variables is in cPanel root
+    $fallback_file = $_SERVER['DOCUMENT_ROOT'] . '/../variables/credentials.php';
+    if (file_exists($fallback_file)) {
+        include_once($fallback_file);
     }
 }
 
